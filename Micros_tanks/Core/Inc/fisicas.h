474933d8c91e45bb21f2b098c52ba0476f28f3ac
Omar Ftouh Labrouzi <80384332+OmarFL@users.noreply.github.com>
2026-01-22 11:54:39 +0100

    Update fisicas.h function signatures for parameters

--- Micros_tanks/Core/Inc/fisicas.h
+++ Micros_tanks/Core/Inc/fisicas.h
@@ -15,15 +15,16 @@ uint32_t* Fisicas_GetBuffer(void);
 void Fisicas_LimpiarPantalla(void);
 
 // Dibujado
-void Fisicas_DibujarEscenario(int t1_x, int t1_y, int t2_x, int t2_y);
+void Fisicas_DibujarEscenario(int t1_x, int t1_y, int t2_x, int t2_y, Nube_t *nubes);
+void Fisicas_DibujarInicio(uint32_t tiempo_actual);
 void Fisicas_DibujarExplosion(int x, int y, uint32_t tiempo_explosion);
 void Fisicas_DibujarGameOver(void);
 void Fisicas_PintarBala(void);
 
 // Lógica
-void Fisicas_PrepararDisparo(int jugador, float angulo, int origen_x, int origen_y);
+void Fisicas_PrepararDisparo(int jugador, float angulo, float fuerza, int origen_x, int origen_y);
 void Fisicas_CalcularSiguientePosicion(void);
-int Fisicas_DetectarColision(int objetivo_x, int objetivo_y);
+int Fisicas_DetectarColision(int objetivo_x, int objetivo_y, Nube_t *nubes);
 int Fisicas_GetBalaX(void);
 int Fisicas_GetBalaY(void);
 
