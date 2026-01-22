2c5cedaaedc4b0e60523e9d2efc5dc62ff59ed0b
Omar Ftouh Labrouzi <80384332+OmarFL@users.noreply.github.com>
2026-01-22 11:52:36 +0100

    Update config.h

--- Micros_tanks/Core/Inc/config.h
+++ Micros_tanks/Core/Inc/config.h
@@ -12,15 +12,15 @@
 #define NUM_MATRICES 8
 
 // --- FÍSICA ---
-#define GRAVEDAD 0.08f        // Ajustado para que se note en pantalla pequeña
-#define VELOCIDAD_DISPARO 2.5f
+#define GRAVEDAD 0.15f        // le he metido más gravedad
+#define VELOCIDAD_DISPARO 3.5f
 
 // --- JUGADORES ---
 #define JUGADOR_1 1
 #define JUGADOR_2 2
 
 // Posiciones iniciales
-#define T1_X_INI  2
+#define T1_X_INI  1
 #define T1_Y_INI  12
 #define T2_X_INI  28
 #define T2_Y_INI  12
@@ -30,4 +30,17 @@
 #define COLISION_OBSTACULO 1
 #define COLISION_TANQUE 2
 
+// --- NUBES ---
+#define MAX_NUBES 4
+
+typedef struct {
+    float x;        // Float para movimiento
+    int y;          // Altura
+    int ancho;      // 2, 3 o 4
+    int alto;       // 1, 2 o 3
+    float velocidad; // 0 = Estática, >0 = Móvil
+    int activa;     // 1 = Sí, 0 = No
+} Nube_t;
+
 #endif /* INC_CONFIG_H_ */
+
