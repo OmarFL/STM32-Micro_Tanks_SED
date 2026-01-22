41e58901c0309c100520174c142c4c53f4eda970
Omar Ftouh Labrouzi <80384332+OmarFL@users.noreply.github.com>
2026-01-22 11:53:08 +0100

    Update drivers_hardware.h

--- Micros_tanks/Core/Inc/drivers_hardware.h
+++ Micros_tanks/Core/Inc/drivers_hardware.h
@@ -15,17 +15,25 @@
 void HW_MAX7219_Init(void);
 void HW_UpdateDisplay(uint32_t *buffer_pantalla);
 
+// --- LEDS de TURNO ---
+void HW_LED_Turno(int jugador); // 1=J1, 2=J2, 0=Apagar
+
 // --- POTENCIÓMETROS (ADC) ---
 // Devuelve el ángulo (0-90) para el jugador indicado (1 o 2)
-float HW_LeerAngulo(int jugador);
+//float HW_LeerAngulo(int jugador);
+// Lee el estado de la mesa de control (Angulo y Potencia)
+// Pasa las variables por referencia para guardarlas
+void HW_LeerControles(float *angulo, float *potencia);
 
 // --- ZUMBADOR (PWM) ---
+void HW_Buzzer_Start(void);
 void HW_Buzzer_Init(void);
 void HW_Buzzer_Stop(void);
-void HW_Buzzer_Frecuencia(float freq); // Pone un tono
-void HW_Buzzer_Disparo(void);          // Efecto de sonido
+void HW_Buzzer_Frecuencia(float freq);
+void HW_Buzzer_Disparo(void);
 void SonidoPWM_Beep(float freq, float duration_ms); //play sonido
 void HW_Buzzer_Victoria(void);         // Melodía completa
+void HW_Buzzer_Impacto(void);
 
 // --- BOTONES ---
 // Devuelve 1 si es una pulsación válida (con debounce), 0 si es ruido
